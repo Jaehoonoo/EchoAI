566cf795442e56c4edee976fd4a5b30fd55a51a6
Jaehoonoo <justjhoon@gmail.com>
2025-10-25 13:45:58 -0400

    initial swift commit

---
+++ EchoAI-client/EchoAI-client/EchoAI_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoAI_clientApp.swift
+//  EchoAI-client
+//
+//  Created by Jhoon Yi on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct EchoAI_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
